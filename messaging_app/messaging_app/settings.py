3079b08e822f4348522cb473623050bb0da37e54
Akindipe Muheez <akindipemuheez@gmail.com>
2025-07-17 19:21:49 +0100

    Rest Framework added

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "chat.apps.ChatConfig",  # Custom app for chat functionality
+    "rest_framework",  # Django REST framework for API support
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,18 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        # 'rest_framework.authentication.TokenAuthentication',  # optional
+        # 'rest_framework_simplejwt.authentication.JWTAuthentication',  # if using JWT
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+        # Change to IsAuthenticatedOrReadOnly or AllowAny as needed
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  # adjust to your needs
+}
