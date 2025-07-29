3e7ba8f04f44d96f625a16bf7ee0f68340c54ac7
Akins-Coded <akindipemuheez@gmail.com>
2025-07-29 09:05:31 +0100

    new messaging app created

---
+++ Django-signals_orm-0x04/messaging/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.auth.contrib import get_user_model
+
+User = get_user_model()
+
+# Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message from {self.sender} to {self.receiver} at {self.timestamp}"
+    
+class Notification(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    read = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Notification for {self.user} regarding message {self.message.id}"
+    
+
--- Django-signals_orm-0x04/messaging_app/settings.py
+++ Django-signals_orm-0x04/messaging_app/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
     # Local apps
     "chats.apps.ChatsConfig",
+    "messaging.apps.MessagingConfig",
 ]
 
 MIDDLEWARE = [
