b1755bca3c6005470bf8e5f51a1de0751552fe15
Akins-Coded <akindipemuheez@gmail.com>
2025-07-30 12:25:01 +0100

    threaded mesages

--- Django-signals_orm-0x04/messaging/urls.py
+++ Django-signals_orm-0x04/messaging/urls.py
@@ -1,18 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (
-    MessageViewSet,
-    NotificationViewSet,
-    MessageHistoryViewSet,
-    DeleteUserView
-)
+from .views import MessageViewSet, NotificationViewSet, MessageHistoryViewSet, DeleteUserView
 
 router = DefaultRouter()
 router.register(r'messages', MessageViewSet, basename='message')
-router.register(r'notifications', NotificationViewSet, basename='notification')
-router.register(r'message-history', MessageHistoryViewSet, basename='messagehistory')
+router.register(r'notifications', NotificationViewSet)
+router.register(r'history', MessageHistoryViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('delete-account/', DeleteUserView.as_view(), name='delete-account'),
+    path('delete-user/', DeleteUserView.as_view(), name='delete-user'),
 ]
