1ad7a48c1c6295cb1feda3b2315d969d08d7afea
Akins-Coded <akindipemuheez@gmail.com>
2025-08-23 05:59:02 +0100

    test_view assert

--- messaging_app/tests/test_views.py
+++ messaging_app/tests/test_views.py
@@ -93,9 +93,13 @@ def test_message_creation_and_filters(api_client, create_user):
     
         # Create a message
     response = api_client.post(
-    url + f"?conversation_id={conversation.conversation_id}",
-    {"message_body": "Hello Bob!"},
-    format="json",
-)
-assert response.status_code == 201
+        url,
+        {
+            "conversation": str(conversation.conversation_id),
+            "message_body": "Hello Bob!",
+        },
+        format="json",
+    )
+    assert response.status_code == 201
+
 
